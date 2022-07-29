e75fb456d80c2c22d8f9b9f01878f75360e169b8
nayeon-jo <nyc0516@naver.com>
2022-07-29 11:48:06 +0900

    요청 매핑 -회원 관리 HTTP API

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -3,49 +3,52 @@ package hello.springmvc.basic.requestmapping;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/mapping/users")
+@RequestMapping("/mapping/users")   //클래스 레벨에 매핑 정보-> 메서드 레벨에서 해당 정보를 조합해 사용
 public class MappingClassController {
-    /**
-     * 회원 목록 조회: GET /users
-     * 회원 등록: POST /users
-     * 회원 조회: GET /users/{userId}
-     * 회원 수정: PATCH /users/{userId}
-     * 회원 삭제: DELETE /users/{userId}
-    * */
 
     /**
+     * 회원 목록 조회
      * GET /mapping/users
      */
     @GetMapping
     public String users() {
+
         return "get users";
     }
     /**
+     * 회원 등록
      * POST /mapping/users
      */
     @PostMapping
     public String addUser() {
+
         return "post user";
     }
     /**
+     * 회원 조회
      * GET /mapping/users/{userId}
      */
     @GetMapping("/{userId}")
     public String findUser(@PathVariable String userId) {
+
         return "get userId=" + userId;
     }
     /**
+     * 회원 수정
      * PATCH /mapping/users/{userId}
      */
     @PatchMapping("/{userId}")
     public String updateUser(@PathVariable String userId) {
+
         return "update userId=" + userId;
     }
     /**
+     * 회원 삭제
      * DELETE /mapping/users/{userId}
      */
     @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable String userId) {
+
         return "delete userId=" + userId;
     }
 }
