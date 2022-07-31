4ed9dd68ef546ef908436fa22ce47bd007748f6c
nayeon-jo <nyc0516@naver.com>
2022-07-31 21:26:23 +0900

    HTTP 응답 - HTTP API, 메시지 바디에 직접 입력

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -8,20 +8,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
-@Controller
+//@Controller
 //@ResponseBody
-//@RestController
+@RestController
 public class ResponseBodyController {
 
     @GetMapping("/response-body-string-v1")
     public void responseBodyV1(HttpServletResponse response) throws IOException
     {
         response.getWriter().write("ok");
+        //서블릿을 직접 다룰 때 처럼 HttpServletResponse객체를 통해 HTTP 메시지 바디에 직접 ok 응답 메시지를 전달
     }
 
 
@@ -31,13 +33,15 @@ public class ResponseBodyController {
      */
     @GetMapping("/response-body-string-v2")
     public ResponseEntity<String> responseBodyV2() {
-        return new ResponseEntity<>("ok", HttpStatus.OK);
+
+        return new ResponseEntity<>("ok", HttpStatus.OK);   // HTTP 응답 코드를 설정
     }
 
 
-    @ResponseBody
+    @ResponseBody   //view를 사용하지 않고, HTTP 메시지 컨버터를 통해서 HTTP 메시지를 직접 입력 가능
     @GetMapping("/response-body-string-v3")
     public String responseBodyV3() {
+
         return "ok";
     }
 
@@ -47,11 +51,13 @@ public class ResponseBodyController {
         HelloData helloData = new HelloData();
         helloData.setUsername("userA");
         helloData.setAge(20);
+
         return new ResponseEntity<>(helloData, HttpStatus.OK);
+        //HTTP 메시지 컨버터를 통해서 JSON 형식으로 변환되어서 반환
     }
 
 
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.OK)  //응답 코드 설정    //동적으로 변경하려면 ResponseEntity 사용
     @ResponseBody
     @GetMapping("/response-body-json-v2")
     public HelloData responseBodyJsonV2() {
