adbdfb03e5a57f6e0c784df674629451f71a8cf8
nayeon-jo <nyc0516@naver.com>
2023-08-21 10:17:48 +0900

    HTTP 요청 - 헤더 조회

--- src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
+++ src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -15,18 +15,15 @@ import java.util.Locale;
 @Slf4j
 @RestController
 public class RequestHeaderController {
+
     @RequestMapping("/headers")
     public String headers(HttpServletRequest request,
                           HttpServletResponse response,
                           HttpMethod httpMethod,    //HTTP 메서드를 조회
                           Locale locale,            // Locale 정보를 조회
                           @RequestHeader MultiValueMap<String, String> headerMap,   //모든 HTTP 헤더를 MultiValueMap 형식으로 조회
-                          /**MultiValueMap
-                                HTTP header, HTTP 쿼리 파라미터와 같이 "하나의 키"에 "여러 값"을 받을 때 사용
-                               (ex keyA=value1&keyA=value2)
-                               */
                           @RequestHeader("host") String host,   //특정 HTTP 헤더를 조회
-                          @CookieValue(value = "myCookie", required = false) String cookie  //특정 쿠키를 조회
+                          @CookieValue(value = "myCookie",required = false) String cookie  //특정 쿠키를 조회
     ) {
         log.info("request={}", request);
         log.info("response={}", response);
@@ -37,4 +34,5 @@ public class RequestHeaderController {
         log.info("myCookie={}", cookie);
         return "ok";
     }
+
 }
