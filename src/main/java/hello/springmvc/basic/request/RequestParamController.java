bcf1d36f145510138eae97e5ca7d3e837c2eb765
nayeon-jo <nyc0516@naver.com>
2023-08-22 17:00:42 +0900

    HTTP 요청 파라미터 - @RequestParam

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -30,11 +30,6 @@ public class RequestParamController {
     }
 
 
-
-    /**
-     * @RequestParam 사용 - 파라미터 이름으로 바인딩
-     * @ResponseBody 추가 - View 조회를 무시하고, HTTP message body에 !직접 해당 내용 입력!
-     */
     @ResponseBody
     @RequestMapping("/request-param-v2")
     public String requestParamV2(
@@ -45,22 +40,16 @@ public class RequestParamController {
         return "ok";
     }
 
-
     @ResponseBody
     @RequestMapping("/request-param-v3")
     public String requestParamV3(
-            //@RequestParam("username") String memberName,
-            //@RequestParam("age") int memberAge
-            /**HTTP 파라미터 이름과 변수 이름 같으면, @RequestParam(name="xx") !생략 가능!*/
-            @RequestParam String username,
+            @RequestParam String username,  //("username") 생략
             @RequestParam int age) {
 
         log.info("username={}, age={}", username, age);
         return "ok";
     }
-    /**
-     * String, int 등의 단순 타입이면  @RequestParam 도 생략 가능!
-     */
+
     @ResponseBody
     @RequestMapping("/request-param-v4")
     public String requestParamV4(String username, int age) {
@@ -68,19 +57,6 @@ public class RequestParamController {
         return "ok";
     }
 
-
-    /**
-     * @RequestParam.required   //파라미터 필수 여부
-     * /request-param -> 필수인 username이 없으므로 예외(400에러)
-     *
-     * 주의! - 파라미터 이름만 사용
-     * /request-param?username= -> 빈문자로 통과
-     *
-     * 주의! - 기본형(primitive)에 null 입력
-     * /request-param
-     * null을 int에 입력하는 것은 불가능!,
-     * 따라서 null을 받을수있는 Integer로 변경해야 함 (또는 defaultValue 사용)
-     */
     @ResponseBody
     @RequestMapping("/request-param-required")
     public String requestParamRequired(
@@ -91,12 +67,6 @@ public class RequestParamController {
         return "ok";
     }
 
-    /**
-     * @RequestParam - defaultValue 사용
-     * 파라미터에 값이 없는 경우 defaultValue 를 사용하면 기본 값을 적용할 수 있다
-     * 참고: defaultValue는 빈 문자의 경우에도 설정한 기본 값이 적용
-     * /request-param?username=
-     */
     @ResponseBody
     @RequestMapping("/request-param-default")
     public String requestParamDefault(
@@ -107,19 +77,12 @@ public class RequestParamController {
         return "ok";
     }
 
-    /**
-     * 파라미터를 Map으로 조회
-     * @RequestParam Map, MultiValueMap
-     * Map(key=value)
-     * MultiValueMap(key=[value1, value2, ...] ex) (key=userIds, value=[id1, id2])
-     */
     @ResponseBody
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
         return "ok";
     }
-
 
 
 
