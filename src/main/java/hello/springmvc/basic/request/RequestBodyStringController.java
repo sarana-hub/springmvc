f053b1be7bfe87b9138b80a1e16871d781d478e9
nayeon-jo <nyc0516@naver.com>
2023-08-23 16:10:42 +0900

    HTTP 요청 메시지 - 단순 텍스트

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -22,6 +22,7 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 @Controller
 public class RequestBodyStringController {
+
     @PostMapping("/request-body-string-v1")
     public void requestBodyString(HttpServletRequest request, HttpServletResponse response) throws IOException {
         ServletInputStream inputStream = request.getInputStream();
@@ -31,53 +32,41 @@ public class RequestBodyStringController {
         response.getWriter().write("ok");
     }
 
-    /**
-     * InputStream(Reader): HTTP 요청 메시지 바디의 내용을 직접 조회
-     * OutputStream(Writer): HTTP 응답 메시지의 바디에 직접 결과 출력
-     */
+
     @PostMapping("/request-body-string-v2")
-    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
+    public void requestBodyStringV2(InputStream inputStream, Writer respWriter) throws IOException {
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
         log.info("messageBody={}", messageBody);
-        responseWriter.write("ok");
+        respWriter.write("ok");
     }
 
-    /**
-     * HttpEntity: HTTP header, body 정보를 편리하게 조회
-     *
-     * HttpEntity 응답에도 사용 가능
-     * - 메시지 바디 정보 직접 반환 (view 조회X)
-     * - 헤더 정보 포함 가능
-     */
+
     @PostMapping("/request-body-string-v3")
     public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) {
+
         String messageBody = httpEntity.getBody();  // 메시지 바디 정보를 직접 조회
         // HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
-
         log.info("messageBody={}", messageBody);
-        return new HttpEntity<>("ok");
+
+        return new HttpEntity<>("ok");  //메시지 바디 정보를 직접 반환
     }
-    /*public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) {
-        String messageBody = httpEntity.getBody();
-        log.info("messageBody={}", messageBody);
-      *//*HttpEntity 를 상속받은 다음 객체들도 같은 기능을 제공한다.
-      RequestEntity: HttpMethod, url 정보가 추가, 요청에서 사용
-      ResponseEntity: HTTP 상태 코드 설정 가능, 응답에서 사용*//*
-        return new ResponseEntity<String>("ok", HttpStatus.CREATED);
-    }*/
+      /*@PostMapping("/request-body-string-v3")
+      public HttpEntity<String> requestBodyStringV3(RequestEntity<String> httpEntity) {
 
-    /**
-     * @RequestBody: HTTP 메시지 바디 정보를 편리하게 조회
-     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
-     *
-     * @ResponseBody: 메시지 바디 정보 직접 반환(view 조회X)
-     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
-     */
-    @ResponseBody
+          String messageBody = httpEntity.getBody();
+          log.info("messageBody={}", messageBody);
+
+          return new ResponseEntity<String>("ok", HttpStatus.CREATED); //(메시지, HTTP 상태 코드)
+      }*/
+
+
+    @ResponseBody   //메시지 바디 정보 직접 반환(view 조회X)
     @PostMapping("/request-body-string-v4")
     public String requestBodyStringV4(@RequestBody String messageBody) {
+
         log.info("messageBody={}", messageBody);
         return "ok";
     }
+
 }
 
